a37f9b5934763f2c59f488c8aacb1e2f85044308
Renopy <93834390+Renopy@users.noreply.github.com>
2024-10-04 15:24:05 +1000

    Delete code directory

--- code/AMSR2_georef.py
+++
@@ -1,9 +0,0 @@
-
-import netCDF4
-import numpy as np
-import tifffile as tff
-import h5py
-
-fp = r'd:\Projects\USA\images\AllData\GW1AM2_202301251804_116A_L1SGRTBR_2220220.h5'
-
-nc = h5py.File(fp)
